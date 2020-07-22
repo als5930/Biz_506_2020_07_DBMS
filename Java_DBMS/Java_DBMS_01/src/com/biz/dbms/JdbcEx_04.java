39a9a01bc8ab5aa0f6bddf03b46bb8ed9e85d620
als5930 <65649302+als5930@users.noreply.github.com>
2020-07-22 18:15:34 +0900

    Update JdbcEx_04.java

--- Java_DBMS/Java_DBMS_01/src/com/biz/dbms/JdbcEx_04.java
+++ Java_DBMS/Java_DBMS_01/src/com/biz/dbms/JdbcEx_04.java
@@ -5,36 +5,37 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-import com.biz.config.DBConnection;
-import com.biz.config.DBContract;
+import com.biz.dbms.config.DBConnection;
+import com.biz.dbms.config.DBContract;
 
 public class JdbcEx_04 {
 
-	
 	public static void main(String[] args) {
-		
-
-		
-		Connection dbConn =DBConnection.getDBConnection();
+		Connection dbConn = DBConnection.getDBConnection();
 		String delete_sql = DBContract.ORDER_DELETE;
-		Scanner scan =new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("삭제할 SEQ ??");
+		System.out.print("삭제할 SEQ ??>> ");
 		String str_seq = scan.nextLine();
-		
-		Long Long_seq = Long.valueOf(str_seq);
+		long long_seq = Long.valueOf(str_seq);
 		
 		PreparedStatement pSt;
-		
 		try {
 			pSt = dbConn.prepareStatement(delete_sql);
+			pSt.setLong(1, long_seq);
+			
+			int ret = pSt.executeUpdate();
+			if(ret  > 0) {
+				System.out.println("삭제 완료!!!");
+			} else {
+				System.out.println("삭제 실패!!!");
+			}
+			pSt.close();
 		} catch (SQLException e) {
-			
-			System.out.println();
-			
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		
 	}
+	
 }
